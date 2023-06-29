c165453913d233957d60c15c3a773a956c9f6ece
on-the-moon <97404500+on-the-moon@users.noreply.github.com>
2023-06-29 12:50:41 -0600

    Solver Function
    uses the get_words function to generate a dictionary containing all possible words for each word length given.
    
    exception raised if letters given don't equal the total length of word lengths given.
    
    todo: generate combinations of words from each set (only 1 word per combination from each set) and check if letters used is equivalent to letters input

--- solver.py
+++ solver.py
@@ -1,10 +1,28 @@
 from nltk.corpus import words
-import itertools
+from itertools import product
 
 
 def get_words(letters, length):
     matching_length_words = set(w for w in words.words() if len(w) == length)
-    possible_words = set(w for w in matching_length_words if all(l in "".join(letters) for l in w))
+    possible_words = set(
+        w for w in matching_length_words if all(l in "".join(letters) for l in w)
+    )
     return possible_words
 
-print(get_words(["h", "o", "r", "s", "t"], 5))
+
+def solver(letters, lengths):
+    if not len(letters) == sum(lengths):
+        raise Exception(
+            "Number of letters must be equal to the total length of all words."
+        )
+    letters_list = sorted(letters)
+    sorted_lengths = sorted(lengths, reverse=True)
+    possible_words_dict = dict()
+    for i in range(len(lengths)):
+        possible_words_dict["word" + str(i)] = get_words(
+            letters_list, sorted_lengths[i]
+        )
+    print(list(product(possible_words_dict.values())))
+
+
+solver("aeiughkanlkitpfhtesnwie", [1, 4, 4, 9, 5])
